3aa9ec5bf71eeb79952968b317544ded555242b8
Jack Wilsdon <jack.wilsdon@gmail.com>
2015-10-05 20:17:08 +0100

    Initial commit

---
+++ GTFSKit/GTFSKit.h
@@ -0,0 +1,19 @@
+//
+//  GTFSKit.h
+//  GTFSKit
+//
+//  Created by Jack Wilsdon on 05/10/2015.
+//  Copyright © 2015 Jack Wilsdon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GTFSKit.
+FOUNDATION_EXPORT double GTFSKitVersionNumber;
+
+//! Project version string for GTFSKit.
+FOUNDATION_EXPORT const unsigned char GTFSKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GTFSKit/PublicHeader.h>
+
+
