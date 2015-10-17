0562067fb4d88f323b9e2e8bcb220bf5a56bde23
rbreslow <breslowrocky@gmail.com>
2015-10-16 20:49:39 -0400

    Printing test results

--- GTFSKitTests/GTFSKitTests.swift
+++ GTFSKitTests/GTFSKitTests.swift
@@ -24,14 +24,14 @@ class GTFSKitTests: XCTestCase {
     func testStopsPerformance() {
         self.measureBlock {
             let parser = CSVParser(lines: ["stop_id,stop_name,stop_desc,stop_lat,stop_lon,stop_url,location_type,parent_station", "S1,Mission St. & Silver Ave.,The stop is located at the southwest corner of the intersection.,37.728631,-122.431282,,,", "S2,Mission St. & Cortland Ave.,The stop is located 20 feet south of Mission St.,37.74103,-122.422482,,,", "S8,24th St. Mission Station,,37.752240,-122.418450,http://www.bart.gov/stations/stationguide/stationoverview_24st.asp,1,"])
-            parser.parse(Stop)
+            print(parser.parse(Stop))
         }
     }
     
     func testCalendarPerformance() {
         self.measureBlock {
             let parser = CSVParser(lines: ["service_id,monday,tuesday,wednesday,thursday,friday,saturday,sunday,start_date,end_date", "WE,0,0,0,0,0,1,1,20060701,20060731", "WD,1,1,1,1,1,0,0,20060701,20060731"])
-            parser.parse(Calendar)
+            print(parser.parse(Calendar))
         }
     }
     
